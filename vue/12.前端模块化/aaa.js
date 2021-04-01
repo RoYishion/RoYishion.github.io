51463513932ca15dde8ed69b97ea451254cb7e30
RoYishion <1577359413@qq.com>
2021-04-01 13:24:44 +0800

    更新

--- vue/12.前端模块化/aaa.js
+++ vue/12.前端模块化/aaa.js
@@ -3,13 +3,38 @@ var age = 18;
 var flag = true;
 
 function sum(num1, num2) {
-    return num1 + num2
-};
+    return num1 + num2;
+}
 
 if (flag) {
     console.log(sum(20, 30));
-};
+}
 
+// 1.导出方式一
 export {
     flag, sum
-};
+}
+
+// 2.导出方式二
+export var num = 1000;
+export var height = 1.8;
+
+// 3.导出函数/类
+export function mul(num1, num2) {
+    return num1 + num2
+}
+
+export class Person{
+    run() {
+        console.log('奔跑');
+    }
+}
+
+// 5.export default  这种导出方式，可以让别的模块导入的时候重命名，且同一个模块只能有一个export default
+
+// const address = '广州';
+// export default address
+
+export default function(argument) {
+    console.log(argument);
+}
--- vue/12.前端模块化/mmm.js
+++ vue/12.前端模块化/mmm.js
@@ -1,6 +1,36 @@
-import {flag, sum} from './aaa.js';
+// 1.导入的对象中定义的变量
+import {flag, Person, sum} from './aaa.js';
 
 if (flag) {
     console.log('小明');
     console.log(sum(20,30));
-};
+}
+
+// 2.直接导入export定义的变量
+import {num, height} from './aaa.js';
+
+console.log(num);
+console.log(height);
+
+// 3.导入export的function
+import {mul} from './aaa.js';
+
+console.log(mul(30,50));
+
+const p = new Person();
+p.run()
+
+// 4.导入export default里的内容
+// import addr from './aaa.js';
+// console.log(addr);
+
+import arg from './aaa.js';
+arg('Hello');
+
+// 5.统一全部导入
+// import {flag, num, sum, height, mul, Person, arg} from './aaa.js'
+
+import * as aaa from './aaa.js'
+
+console.log(aaa.flag);
+console.log(aaa.height);
